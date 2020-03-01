a7c1aebf49fd73a607f16005abd58285857ca384
Bo Byrd <47284159+tezzigator@users.noreply.github.com>
2020-02-29 20:32:16 -0600

    argv[1] to be the prefix string you desire

---
+++ vanity.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+ 
+from hashlib import sha256, blake2b
+from base58check import b58encode
+import nacl.signing
+from sys import argv
+
+edsklongprefix = bytes.fromhex('2bf64e07') # decimal is { (byte) 43, (byte) 246, (byte) 78, (byte) 7 };
+edpkPrefix = bytes.fromhex('0d0f25d9') # decimal is { (byte) 13, (byte) 15, (byte) 37, (byte) 217 };
+tz1prefix=  bytes.fromhex('06a19f') # decimal is { (byte) 6, (byte) 161, (byte) 159 };
+
+while 1:
+
+    signing_key = nacl.signing.SigningKey.generate()
+    verify_key = signing_key.verify_key
+    publicbytes = verify_key.encode()
+    privatebytes = signing_key.encode() + publicbytes
+
+    prefixedprivatebytes = edsklongprefix + privatebytes
+    shabytes = sha256(sha256(prefixedprivatebytes).digest()).digest()[:4]
+    edsk = b58encode(prefixedprivatebytes+shabytes).decode()
+
+    prefixedpub = edpkPrefix + publicbytes
+    shabytes = sha256(sha256(prefixedpub).digest()).digest()[:4]
+
+    blake2bhash = blake2b(publicbytes, digest_size=20).digest()
+    shabytes = sha256(sha256(tz1prefix + blake2bhash).digest()).digest()[:4]
+    pkhash = b58encode(tz1prefix + blake2bhash + shabytes).decode()
+
+    if pkhash[3:len(argv[1])+3] == argv[1]:
+        print(edsk)
+        print(pkhash)
+        break

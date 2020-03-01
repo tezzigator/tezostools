2b10b2a92640f343658bd61b04c24b61bce55c28
Bo Byrd <47284159+tezzigator@users.noreply.github.com>
2020-02-29 22:32:07 -0600

    Update vanity.py

--- vanity.py
+++ vanity.py
@@ -24,5 +24,6 @@ for a in range (0, 999999999):
         edsk = b58encode(prefixedprivatebytes+shabytes).decode()
         print(edsk)
         print(pkhash)
-        if a % 10000 == 0: print(str(a))
         break
+    
+    if a % 10000 == 0: print(str(a))
